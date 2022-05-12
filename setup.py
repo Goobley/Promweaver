28b71c71ba10049be7555471e3b3e538c7b4e99a
cmo <c.osborne.1@research.gla.ac.uk>
2022-05-12 21:43:37 +0100

    Initial install scripts, may need further modification

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+def readme():
+    with open("README.md", "r") as f:
+        return f.read()
+
+setup(
+    name="promweaver",
+    setup_requires=["setuptools_scm"],
+    use_scm_version=True,
+    packages=["promweaver"],
+    install_requires=["lightweaver>=0.8.0rc6"],
+    author="Chris Osborne",
+    author_email="christopher.osborne@glasgow.ac.uk",
+    license="MIT",
+    url="http://github.com/Goobley/Promweaver",
+    description="Prominence/Filament Radiative Transfer Modelling with Lightweaver",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    python_requires=">=3.8"
+)
